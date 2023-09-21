6a023a3306208ebb5dae9b95f263a65f6ba85a61
giodueck <giodueck@gmail.com>
2023-09-21 16:59:52 -0400

    Add LSP for LaTeX

--- init.lua
+++ init.lua
@@ -567,4 +567,9 @@ require('onedark').load()
 vim.cmd("syntax enable")
 
 vim.cmd("let g:vimtex_view_method = 'zathura'")
-
+vim.cmd("let g:vimtex_fold_enabled = 0")
+vim.cmd("let g:vimtex_indent_enabled = 1")
+vim.cmd("let g:vimtex_complete_recursive_bib = 0")
+vim.cmd("let g:vimtex_complete_close_braces = 1")
+vim.cmd("let g:vimtex_quickfix_mode = 2")
+vim.cmd("let g:vimtex_quickfix_open_on_warning = 1")
