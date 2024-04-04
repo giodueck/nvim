917bf3fd8b425f9269ba242701b83367ad980cbb
Giovanni Dueck <giovanni.dueck@avantys.de>
2024-04-04 10:56:57 -0400

    Add some telescope mappings

--- lua/plugins/telescope-file-browser.lua
+++
@@ -1,8 +0,0 @@
-require('telescope').setup {
-  extensions = {
-    file_browser = {
-        -- use the "ivy" theme if you want
-        theme = "ivy",
-    }
-  }
-}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,23 @@
+local actions = require "telescope.actions"
+local action_layout = require("telescope.actions.layout")
 require('telescope').setup {
   defaults = {
     mappings = {
       i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
+        ["<esc>"] = actions.close,
+        ["<M-p>"] = action_layout.toggle_preview,
+      },
+      n = {
+        ["<M-p>"] = action_layout.toggle_preview,
+      }
+    },
+  },
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ["<M-d>"] = actions.delete_buffer + actions.move_to_top,
+        },
       },
     },
   },
