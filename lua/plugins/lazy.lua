53f2d4665687f8d05b9b8674d05ca0aeab8fa1cc
Giovanni Dueck <giovanni.dueck@avantys.de>
2024-05-16 08:07:16 -0400

    Add debugprint.nvim

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -259,4 +259,54 @@ require('lazy').setup({
     "roxma/vim-tmux-clipboard"
   },
 
+  -- Debug print helper
+  {
+    "andrewferrier/debugprint.nvim",
+    dependencies = {
+        "echasnovski/mini.nvim", -- Needed to enable :ToggleCommentDebugPrints for NeoVim <= 0.9
+        "nvim-treesitter/nvim-treesitter" -- Needed to enable treesitter for NeoVim 0.8
+    },
+    opts = {
+      keymaps = {
+        normal = {
+          plain_below = "g?p",
+          plain_above = "g?P",
+          variable_below = "g?v",
+          variable_above = "g?V",
+          variable_below_alwaysprompt = nil,
+          variable_above_alwaysprompt = nil,
+          textobj_below = "g?o",
+          textobj_above = "g?O",
+          toggle_comment_debug_prints = "g?t",
+          delete_debug_prints = "g?d",
+        },
+        visual = {
+          variable_below = "g?v",
+          variable_above = "g?V",
+        },
+      },
+      commands = {
+        toggle_comment_debug_prints = "ToggleCommentDebugPrints",
+        delete_debug_prints = "DeleteDebugPrints",
+      },
+    },
+    -- The 'keys' and 'cmds' sections of this configuration are only needed if
+    -- you want to take advantage of `lazy.nvim` lazy-loading.
+    keys = {
+      { "g?p", mode = 'n' },
+      { "g?P", mode = 'n' },
+      { "g?v", mode = 'n' },
+      { "g?V", mode = 'n' },
+      { "g?o", mode = 'n' },
+      { "g?O", mode = 'n' },
+      { "g?v", mode = 'x' },
+      { "g?V", mode = 'x' },
+    },
+    cmd = {
+      "ToggleCommentDebugPrints",
+      "DeleteDebugPrints",
+    },
+    version = "*"
+  },
+
 }, {})
