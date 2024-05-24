0976b7c1e7b8dcef20a2059800eaa90e941c41e6
Giovanni Dueck <giovanni.dueck@avantys.de>
2024-05-24 09:57:43 -0400

    Add open multiple selected buffers to telescope pickers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,33 @@
 local actions = require "telescope.actions"
 local action_layout = require("telescope.actions.layout")
+
+-- Selects all selected buffers, or the one under the curser if none are selected
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 require('telescope').setup {
   defaults = {
     mappings = {
       i = {
         ["<esc>"] = actions.close,
         ["<M-p>"] = action_layout.toggle_preview,
+        ["<CR>"] = select_one_or_multi,
       },
       n = {
         ["<M-p>"] = action_layout.toggle_preview,
+        ["<CR>"] = select_one_or_multi,
       }
     },
     prompt_prefix = ' ',
