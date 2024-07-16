0edd16cc4f57a6cb250cebc5a088837905c5418c
Giovanni Dueck <giovanni.dueck@avantys.de>
2024-07-16 08:32:12 -0400

    Made tmux-navigate keymaps silent

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>",
-  { noremap = true, desc = "Tmux: navigate left" })
+  { noremap = true, desc = "Tmux: navigate left", silent = true })
 vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>",
-  { noremap = true, desc = "Tmux: navigate down" })
+  { noremap = true, desc = "Tmux: navigate down", silent = true })
 vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>",
-  { noremap = true, desc = "Tmux: navigate up" })
+  { noremap = true, desc = "Tmux: navigate up", silent = true })
 vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>",
-  { noremap = true, desc = "Tmux: navigate right" })
+  { noremap = true, desc = "Tmux: navigate right", silent = true })
 vim.keymap.set("n", "<C-\\>", ":TmuxNavigatePrevious<CR>",
-  { noremap = true, desc = "Tmux: navigate previous" })
+  { noremap = true, desc = "Tmux: navigate previous", silent = true })
