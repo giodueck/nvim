7cb55ca09e45d079b5d6b42e9319e9d5a3d93bda
Giovanni Dueck <giovanni.dueck@avantys.de>
2025-08-28 11:02:23 -0300

    Hex: make bind less easy to mistype

--- lua/plugins/hex.lua
+++ lua/plugins/hex.lua
@@ -1,7 +1,7 @@
 require 'hex'.setup()
 vim.api.nvim_set_keymap(
   "n",
-  "<leader>x",
+  "<leader>xx",
   "<cmd>lua require 'hex'.toggle()<CR>",
   { noremap = true, desc = 'Toggle Hex Editor', silent = true }
 )
