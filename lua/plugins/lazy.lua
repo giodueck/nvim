e5c8d07ea1bfaee6a023c064ea9c2508de37b424
Giovanni Dueck <giovanni.dueck@avantys.de>
2025-12-04 07:58:51 -0300

    Lazy: format file and add some configurations to lazy itself

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -13,325 +13,331 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Install plugins with lazy.nvim
 require('lazy').setup({
-  -- NOTE: First, some plugins that don't require any configuration
+    -- NOTE: First, some plugins that don't require any configuration
 
-  -- Git related plugins
-  'tpope/vim-fugitive',
+    -- Git related plugins
+    'tpope/vim-fugitive',
 
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+    -- Detect tabstop and shiftwidth automatically
+    'tpope/vim-sleuth',
 
-  -- Surround text in " ' { [ ( or html tags
-  'tpope/vim-surround',
-  -- Make the '.' command work with vim-surround
-  'tpope/vim-repeat',
+    -- Surround text in " ' { [ ( or html tags
+    'tpope/vim-surround',
+    -- Make the '.' command work with vim-surround
+    'tpope/vim-repeat',
 
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
-  { -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      'mason-org/mason.nvim',
-      'mason-org/mason-lspconfig.nvim',
+    -- NOTE: This is where your plugins related to LSP can be installed.
+    --  The configuration is done below. Search for lspconfig to find it below.
+    { -- LSP Configuration & Plugins
+      'neovim/nvim-lspconfig',
+      dependencies = {
+        -- Automatically install LSPs to stdpath for neovim
+        'mason-org/mason.nvim',
+        'mason-org/mason-lspconfig.nvim',
 
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      {
-        'j-hui/fidget.nvim',
-        tag = "legacy",
-        opts = {}
-      },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      -- 'folke/neodev.nvim', -- this is now deprecated in favor of lazydev
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = "luvit-meta/library", words = { "vim%.uv" } },
-          },
-        },
-      },
-      { "Bilal2453/luvit-meta",      lazy = true }, -- optional `vim.uv` typings
-      {                                             -- optional completion source for require statements and module annotations
-        "hrsh7th/nvim-cmp",
-        opts = function(_, opts)
-          opts.sources = opts.sources or {}
-          table.insert(opts.sources, {
-            name = "lazydev",
-            group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-          })
-        end,
-      },
-
-      -- In an attempt to make Go LSP work
-      { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
-    },
-  },
-
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  },
-
-  -- Useful plugin to show you pending keybinds.
-  {
-    'folke/which-key.nvim',
-    opts = {
-      expand = 1,
-      delay = 500,
-    },
-    dependencies = { 'nvim-tree/nvim-web-devicons', },
-  },
-
-  { -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
-  -- Tokyo Night theme
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-
-  { -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'tokyonight',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        always_show_tabline = false,
-      },
-      tabline = {
-        lualine_b = {
-          {
-            'tabs', mode = 2, max_length = vim.o.columns,
-          },
-        },
-      },
-    },
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    lazy = false,
-  },
-
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    opts = {},
-  },
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',         opts = {} },
-
-  -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
-
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-  -- Only load if `make` is available. Make sure you have the system
-  -- requirements installed.
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    -- NOTE: If you are having trouble with this installation,
-    --       refer to the README for telescope-fzf-native for more instructions.
-    build = 'make',
-    cond = function()
-      return vim.fn.executable 'make' == 1
-    end,
-  },
-
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    config = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
-  },
-
-  -- Hex editor
-  {
-    'RaafatTurki/hex.nvim',
-    opts = {
-      is_file_binary_post_read = function() return false end
-    },
-  },
-
-  -- Telescope file browser
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-  },
-
-  -- LaTeX functonality
-  {
-    "lervag/vimtex",
-    init = function()
-      --vim.g.vimtex_options = "go here"
-    end,
-    dependencies = { "micangl/cmp-vimtex" },
-  },
-
-  -- Obsidian
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
-    -- ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    event = {
-      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-      "BufReadPre " .. vim.fn.expand "~" .. "/Obsidian/**.md",
-      "BufNewFile " .. vim.fn.expand "~" .. "/Obsidian/**.md",
-    },
-    dependencies = {
-      -- Required.
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-treesitter",
-    },
-    opts = {
-      workspaces = {
+        -- Useful status updates for LSP
+        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
         {
-          name = "buf-parent",
-          path = function()
-            return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+          'j-hui/fidget.nvim',
+          tag = "legacy",
+          opts = {}
+        },
+
+        -- Additional lua configuration, makes nvim stuff amazing!
+        -- 'folke/neodev.nvim', -- this is now deprecated in favor of lazydev
+        {
+          "folke/lazydev.nvim",
+          ft = "lua", -- only load on lua files
+          opts = {
+            library = {
+              -- See the configuration section for more details
+              -- Load luvit types when the `vim.uv` word is found
+              { path = "luvit-meta/library", words = { "vim%.uv" } },
+            },
+          },
+        },
+        { "Bilal2453/luvit-meta",      lazy = true }, -- optional `vim.uv` typings
+        {                                           -- optional completion source for require statements and module annotations
+          "hrsh7th/nvim-cmp",
+          opts = function(_, opts)
+            opts.sources = opts.sources or {}
+            table.insert(opts.sources, {
+              name = "lazydev",
+              group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+            })
           end,
         },
+
+        -- In an attempt to make Go LSP work
+        { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
       },
+    },
 
-      disable_frontmatter = true,
+    { -- Autocompletion
+      'hrsh7th/nvim-cmp',
+      dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    },
 
-      templates = {
-        subdir = "Templates",
-        date_format = "%Y-%m-%d",
-        time_format = "%H:%M",
-        -- A map for custom variables, the key should be the variable and the value a function
-        substitutions = {},
+    -- Useful plugin to show you pending keybinds.
+    {
+      'folke/which-key.nvim',
+      opts = {
+        expand = 1,
+        delay = 500,
       },
+      dependencies = { 'nvim-tree/nvim-web-devicons', },
+    },
 
-      note_id_func = function(title)
-        if title ~= nil then
-          return title
-        else
-          return tostring(os.time())
-        end
+    { -- Adds git releated signs to the gutter, as well as utilities for managing changes
+      'lewis6991/gitsigns.nvim',
+      opts = {
+        -- See `:help gitsigns.txt`
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+      },
+    },
+
+    -- Tokyo Night theme
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {},
+    },
+
+    { -- Set lualine as statusline
+      'nvim-lualine/lualine.nvim',
+      -- See `:help lualine.txt`
+      opts = {
+        options = {
+          icons_enabled = true,
+          theme = 'tokyonight',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          always_show_tabline = false,
+        },
+        tabline = {
+          lualine_b = {
+            {
+              'tabs', mode = 2, max_length = vim.o.columns,
+            },
+          },
+        },
+      },
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      lazy = false,
+    },
+
+    { -- Add indentation guides even on blank lines
+      'lukas-reineke/indent-blankline.nvim',
+      main = 'ibl',
+      opts = {},
+    },
+
+    -- "gc" to comment visual regions/lines
+    { 'numToStr/Comment.nvim',         opts = {} },
+
+    -- Fuzzy Finder (files, lsp, etc)
+    { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
+
+    -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+    -- Only load if `make` is available. Make sure you have the system
+    -- requirements installed.
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      -- NOTE: If you are having trouble with this installation,
+      --       refer to the README for telescope-fzf-native for more instructions.
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
       end,
+    },
 
-      ui = {
-        checkboxes = {
-          -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-          ["x"] = { char = "", hl_group = "ObsidianDone" },
-          [">"] = { char = "", hl_group = "ObsidianRightArrow" },
-          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
-          ["/"] = { char = "󰡖", hl_group = "ObsidianDone" },
+    { -- Highlight, edit, and navigate code
+      'nvim-treesitter/nvim-treesitter',
+      dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+      },
+      config = function()
+        pcall(require('nvim-treesitter.install').update { with_sync = true })
+      end,
+    },
+
+    -- Hex editor
+    {
+      'RaafatTurki/hex.nvim',
+      opts = {
+        is_file_binary_post_read = function() return false end
+      },
+    },
+
+    -- Telescope file browser
+    {
+      "nvim-telescope/telescope-file-browser.nvim",
+      dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+
+    -- LaTeX functonality
+    {
+      "lervag/vimtex",
+      init = function()
+        --vim.g.vimtex_options = "go here"
+      end,
+      dependencies = { "micangl/cmp-vimtex" },
+    },
+
+    -- Obsidian
+    {
+      "epwalsh/obsidian.nvim",
+      version = "*", -- recommended, use latest release instead of latest commit
+      lazy = true,
+      -- ft = "markdown",
+      -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+      event = {
+        -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+        -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+        "BufReadPre " .. vim.fn.expand "~" .. "/Obsidian/**.md",
+        "BufNewFile " .. vim.fn.expand "~" .. "/Obsidian/**.md",
+      },
+      dependencies = {
+        -- Required.
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+        "nvim-treesitter",
+      },
+      opts = {
+        workspaces = {
+          {
+            name = "buf-parent",
+            path = function()
+              return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+            end,
+          },
+        },
+
+        disable_frontmatter = true,
+
+        templates = {
+          subdir = "Templates",
+          date_format = "%Y-%m-%d",
+          time_format = "%H:%M",
+          -- A map for custom variables, the key should be the variable and the value a function
+          substitutions = {},
+        },
+
+        note_id_func = function(title)
+          if title ~= nil then
+            return title
+          else
+            return tostring(os.time())
+          end
+        end,
+
+        ui = {
+          checkboxes = {
+            -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+            ["x"] = { char = "", hl_group = "ObsidianDone" },
+            [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+            ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+            ["/"] = { char = "󰡖", hl_group = "ObsidianDone" },
+          },
         },
       },
     },
-  },
 
-  -- Vim Tmux navigator
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-
-  -- Fast and simple frequent file navigation
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
-
-  -- AsyncTasks: VSCode-like tasks
-  {
-    "skywind3000/asynctasks.vim",
-    dependencies = { "skywind3000/asyncrun.vim" },
-  },
-
-  -- Telescope async tasks extension
-  {
-    "GustavoKatel/telescope-asynctasks.nvim",
-    lazy = true,
-  },
-
-  -- Debug print helper
-  {
-    "andrewferrier/debugprint.nvim",
-    dependencies = {
-      "nvim-mini/mini.hipatterns",     -- Optional: Needed for line highlighting ('fine-grained' hipatterns plugin)
-      "nvim-telescope/telescope.nvim", -- Optional: If you want to use the `:Debugprint search` command with telescope.nvim
+    -- Vim Tmux navigator
+    {
+      "christoomey/vim-tmux-navigator",
+      lazy = false,
     },
-    opts = {
-      keymaps = {
-        normal = {
-          plain_below = "g?p",
-          plain_above = "g?P",
-          variable_below = "g?v",
-          variable_above = "g?V",
-          variable_below_alwaysprompt = nil,
-          variable_above_alwaysprompt = nil,
-          textobj_below = "g?o",
-          textobj_above = "g?O",
-          toggle_comment_debug_prints = "g?t",
-          delete_debug_prints = "g?d",
+
+    -- Fast and simple frequent file navigation
+    {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim" }
+    },
+
+    -- AsyncTasks: VSCode-like tasks
+    {
+      "skywind3000/asynctasks.vim",
+      dependencies = { "skywind3000/asyncrun.vim" },
+    },
+
+    -- Telescope async tasks extension
+    {
+      "GustavoKatel/telescope-asynctasks.nvim",
+      lazy = true,
+    },
+
+    -- Debug print helper
+    {
+      "andrewferrier/debugprint.nvim",
+      dependencies = {
+        "nvim-mini/mini.hipatterns",   -- Optional: Needed for line highlighting ('fine-grained' hipatterns plugin)
+        "nvim-telescope/telescope.nvim", -- Optional: If you want to use the `:Debugprint search` command with telescope.nvim
+      },
+      opts = {
+        keymaps = {
+          normal = {
+            plain_below = "g?p",
+            plain_above = "g?P",
+            variable_below = "g?v",
+            variable_above = "g?V",
+            variable_below_alwaysprompt = nil,
+            variable_above_alwaysprompt = nil,
+            textobj_below = "g?o",
+            textobj_above = "g?O",
+            toggle_comment_debug_prints = "g?t",
+            delete_debug_prints = "g?d",
+          },
+          visual = {
+            variable_below = "g?v",
+            variable_above = "g?V",
+          },
         },
-        visual = {
-          variable_below = "g?v",
-          variable_above = "g?V",
+        commands = {
+          toggle_comment_debug_prints = "ToggleCommentDebugPrints",
+          delete_debug_prints = "DeleteDebugPrints",
         },
       },
-      commands = {
-        toggle_comment_debug_prints = "ToggleCommentDebugPrints",
-        delete_debug_prints = "DeleteDebugPrints",
+      -- The 'keys' and 'cmds' sections of this configuration are only needed if
+      -- you want to take advantage of `lazy.nvim` lazy-loading.
+      keys = {
+        { "g?p", mode = 'n' },
+        { "g?P", mode = 'n' },
+        { "g?v", mode = 'n' },
+        { "g?V", mode = 'n' },
+        { "g?o", mode = 'n' },
+        { "g?O", mode = 'n' },
+        { "g?v", mode = 'x' },
+        { "g?V", mode = 'x' },
+        { "g?d", mode = 'n' },
+      },
+      cmd = {
+      },
+      version = "*"
+    },
+
+    {
+      "elkowar/yuck.vim",
+      lazy = true,
+      event = {
+        "BufReadPre " .. "*.yuck",
+        "BufNewFile " .. "*.yuck",
       },
     },
-    -- The 'keys' and 'cmds' sections of this configuration are only needed if
-    -- you want to take advantage of `lazy.nvim` lazy-loading.
-    keys = {
-      { "g?p", mode = 'n' },
-      { "g?P", mode = 'n' },
-      { "g?v", mode = 'n' },
-      { "g?V", mode = 'n' },
-      { "g?o", mode = 'n' },
-      { "g?O", mode = 'n' },
-      { "g?v", mode = 'x' },
-      { "g?V", mode = 'x' },
-      { "g?d", mode = 'n' },
-    },
-    cmd = {
-    },
-    version = "*"
-  },
 
+  },
   {
-    "elkowar/yuck.vim",
-    lazy = true,
-    event = {
-      "BufReadPre " .. "*.yuck",
-      "BufNewFile " .. "*.yuck",
+    rocks = {
+      enabled = false,
+      hererocks = false,
     },
-  },
-
-}, {})
+  })
