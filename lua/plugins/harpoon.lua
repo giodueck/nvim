8cb78bf33acf3551d535ddaf7dc423b2192c4545
Giovanni Dueck <giodueck@gmail.com>
2026-06-26 19:01:55 -0300

    Harpoon: add alt-[asdfg] for 1st-5th entry selection

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -15,6 +15,12 @@ vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end, { desc = '
 vim.keymap.set("n", "<M-5>", function() harpoon:list():select(5) end, { desc = 'Harpoon: Go to fifth harpoon item' })
 vim.keymap.set("n", "<M-6>", function() harpoon:list():select(6) end, { desc = 'Harpoon: Go to sixth harpoon item' })
 
+vim.keymap.set("n", "<M-a>", function() harpoon:list():select(1) end, { desc = 'Harpoon: Go to first harpoon item' })
+vim.keymap.set("n", "<M-s>", function() harpoon:list():select(2) end, { desc = 'Harpoon: Go to second harpoon item' })
+vim.keymap.set("n", "<M-d>", function() harpoon:list():select(3) end, { desc = 'Harpoon: Go to third harpoon item' })
+vim.keymap.set("n", "<M-f>", function() harpoon:list():select(4) end, { desc = 'Harpoon: Go to fourth harpoon item' })
+vim.keymap.set("n", "<M-g>", function() harpoon:list():select(5) end, { desc = 'Harpoon: Go to fifth harpoon item' })
+
 -- Toggle previous & next buffers stored within Harpoon list (I never use these)
 -- vim.keymap.set("n", "<C-P>", function() harpoon:list():prev() end)
 -- vim.keymap.set("n", "<C-N>", function() harpoon:list():next() end)
